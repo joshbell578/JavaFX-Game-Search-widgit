72517ab251dbb120a1ff6ded340ec651334082a3
Loera_Rodrigo <loerr578@hsd.k12.or.us>
2023-01-25 15:23:22 -0800

    Changing form mat

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -4,19 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Window;
 
 public class PrimaryController {
     @FXML
-    private TextField nameField;
+    private TextField searchField;
 
     @FXML
-    private TextField emailField;
-
-    @FXML
-    private PasswordField passwordField;
+    private 
 
     @FXML
     private Button submitButton;
@@ -24,23 +20,8 @@ public class PrimaryController {
     @FXML
     protected void handleSubmitButtonAction(ActionEvent event) {
         Window owner = submitButton.getScene().getWindow();
-        if(nameField.getText().isEmpty()) {
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
-                    "Please enter your name");
-            return;
-        }
-        if(emailField.getText().isEmpty()) {
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
-                    "Please enter your email id");
-            return;
-        }
-        if(passwordField.getText().isEmpty()) {
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
-                    "Please enter a password");
-            return;
-        }
 
         AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
-                "Welcome " + nameField.getText());
+                "Welcome ");
     }
 }
